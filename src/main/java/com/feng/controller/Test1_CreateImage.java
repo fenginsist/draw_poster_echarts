0935bab75bb86c166fa8de1641d7df9d5f82488a
fengfl <fengfl@dataadt.com>
2021-06-15 16:13:48 +0800

    20210615：冯凡利：init

---
+++ src/main/java/com/feng/controller/Test1_CreateImage.java
@@ -0,0 +1,25 @@
+package com.feng.controller;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class Test1_CreateImage {
+
+    /**
+     * @Author fengfanli
+     * @Description //TODO 生成一个白色的图片
+     * @Date 15:51 2021/4/8
+     * @Param [args]
+     * @return void
+     **/
+    public static void main(String[] args) throws Exception {
+        BufferedImage bufIma = new BufferedImage( 50, 50, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufIma.createGraphics();
+        g2d.setBackground(new Color(255, 255, 255));
+        g2d.clearRect(0, 0, 50, 50);
+        ImageIO.write(bufIma, "png", new File("d:\\6.png"));
+
+    }
+}
